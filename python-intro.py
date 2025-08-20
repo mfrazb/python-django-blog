de4701f7e28ec73204ee3380cc884139987d6140
Meredith Frazier Britt <118227496+mfrazb@users.noreply.github.com>
2025-08-20 14:21:44 -0500

    chore: update files after pre-commit-hooks was run on repo

--- python-intro.py
+++ python-intro.py
@@ -1,47 +1,48 @@
-name = 'Meredith'
-name3Times = 'Meredith' * 3
+name = "Meredith"
+name3Times = "Meredith" * 3
 print(name)
 print(name3Times)
 
 # list
-names = ['Sara', 'Meredith', 'Shavone', 'Deanna', 'Peyton']
+names = ["Sara", "Meredith", "Shavone", "Deanna", "Peyton"]
 
-# practice with if else 
+# practice with if else
 if 2 > 3:
-  print('You got it girl')
-else: 
-  print('not so much')
-
-# practice with else if 
-if 'Dog' in names: 
-  print('Dog is in the list')
-elif 'Meredit' in names:
-  print ('Meredith is in the list')
+    print("You got it girl")
 else:
-  print ('No names in list')
+    print("not so much")
+
+# practice with else if
+if "Dog" in names:
+    print("Dog is in the list")
+elif "Meredit" in names:
+    print("Meredith is in the list")
+else:
+    print("No names in list")
 
 # functions and parameters
 def hi(name):
-  print(f'Hello {name}!')
+    print(f"Hello {name}!")
+
 
 # function execution
-hi('Sandy')
+hi("Sandy")
 
-# loops 
+# loops
 # for name in names:
 #   hi(name)
 #   print('Next name')
 
-# loops with numbers 
+# loops with numbers
 for i in range(1, 6):
-  print(i)
+    print(i)
 
 # loop based on list index
 for i in range(len(names) - 2):
-  print(names[i])
+    print(names[i])
 
 # while loops
 i = 0
 while i < 10:
-  print(i)
-  i += 1
+    print(i)
+    i += 1
