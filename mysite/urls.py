6818bf8d6440d75df8ba4c64de40784a3a5118d9
Meredith Frazier Britt <118227496+mfrazb@users.noreply.github.com>
2025-08-21 12:44:29 -0500

    style: update end pt for STATIC_ROOT

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,5 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include("blog.urls"))]
+
+# Add static files serving for development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
